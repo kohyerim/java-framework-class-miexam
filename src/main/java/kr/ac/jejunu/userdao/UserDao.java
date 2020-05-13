58e1e933a6816ea8b60d4b9602aa6785f59e69a5
kohyerim <kohyerim78@gmail.com>
2020-05-13 17:27:03 +0900

    실습과제 1: 사용자추가기능

--- src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -3,11 +3,8 @@ package kr.ac.jejunu.userdao;
 import java.sql.*;
 
 public class UserDao {
-    public User get(Integer id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection =
-                DriverManager.getConnection("jdbc:mysql://localhost/jeju?serverTimezone=UTC"
-                , "jeju", "jejupw");
+    public User get(Integer id) throws SQLException, ClassNotFoundException {
+        Connection connection = getConnection();
         PreparedStatement preparedStatement =
                 connection.prepareStatement("select * from userinfo where id = ?");
         preparedStatement.setLong(1, id);
@@ -25,5 +22,25 @@ public class UserDao {
         connection.close();
         //리턴
         return user;
+    }
+
+    private Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost/mydb?serverTimezone=UTC", "hyerim", "1234");
+    }
+
+    public void insert(User user) throws SQLException, ClassNotFoundException {
+        Connection connection = getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("insert into userinfo (name, paswword) values (?, ?)");
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getPassword());
+        preparedStatement.executeUpdate();
+
+        ResultSet resultSet = preparedStatement.getGeneratedKeys();
+        resultSet.next();
+        user.setId(resultSet.getInt(1));
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
     }
 }
