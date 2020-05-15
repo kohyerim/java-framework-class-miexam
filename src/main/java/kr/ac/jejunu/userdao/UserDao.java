274602e04c0b07205cc108018ffa8bda164abebc
kohyerim <kohyerim78@gmail.com>
2020-05-15 09:17:36 +0900

    실습과제 10: Context 클래스로 분류 수정

--- src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -1,8 +1,5 @@
 package kr.ac.jejunu.userdao;
 
-import javax.sql.DataSource;
-import java.sql.*;
-
 public class UserDao {
 
     private final JdbcContext jdbcContext;
@@ -13,35 +10,15 @@ public class UserDao {
 
     public User get(Integer id) {
         StatementStrategy statementStrategy = new GetStatementStrategy(id);
-        //리턴
         return jdbcContext.jdbcContextForGet(statementStrategy);
     }
 
-    public void insert(User user) throws SQLException {
+    public void insert(User user) {
         StatementStrategy statementStrategy = new InsertStatementStrategy(user);
         jdbcContext.jdbcContextForInsert(user, statementStrategy);
     }
 
-    public void update(User user) throws SQLException{
-//        Connection connection = null;
-//        PreparedStatement preparedStatement = null;
-//
-//        try {
-//            connection = dataSource.getConnection();
-//            StatementStrategy statementStrategy = new UpdateStatementStrategy();
-//            preparedStatement = statementStrategy.makeStrategy(user, connection);
-//        } finally {
-//            try {
-//                preparedStatement.close();
-//            } catch (SQLException e) {
-//                e.printStackTrace();
-//            }
-//            try {
-//                connection.close();
-//            } catch (SQLException e) {
-//                e.printStackTrace();
-//            }
-//        }
+    public void update(User user) {
         StatementStrategy statementStrategy = new UpdateStatementStrategy(user);
         jdbcContext.jdbcContextForUpdateAndDelete(statementStrategy);
     }
