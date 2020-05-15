38c91adad3f82b15a8185920d0c91b9765102ff0
kohyerim <kohyerim78@gmail.com>
2020-05-15 10:03:36 +0900

    실습과제 13: JdbcTemplate

--- src/main/java/kr/ac/jejunu/userdao/UserDao.java
+++ src/main/java/kr/ac/jejunu/userdao/UserDao.java
@@ -1,34 +1,61 @@
 package kr.ac.jejunu.userdao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+import java.lang.reflect.GenericArrayType;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.PropertyResourceBundle;
+
 public class UserDao {
 
-    private final JdbcContext jdbcContext;
+    private final JdbcTemplate jdbcTemplate;
 
-    protected UserDao(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
+    protected UserDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public User get(Integer id) {
         Object[] params = new Object[]{id};
         String sql = "select id, name, password from userinfo where id = ?";
-        return jdbcContext.get(sql, params);
+        return jdbcTemplate.query(sql, params, rs -> {
+            User user = null;
+            if(rs.next()){
+                user = new User();
+                user.setId(rs.getInt("id"));
+                user.setName((rs.getString("name")));
+                user.setPassword((rs.getString("password")));
+                return user;
+            }
+            return user;
+        });
     }
 
     public void insert(User user) {
         Object[] params = new Object[]{user.getName(), user.getPassword()};
         String sql = "insert into userinfo (name, password) value (?, ?)";
-        jdbcContext.insert(user, params, sql);
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(con -> {
+            PreparedStatement preparedStatement = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            for(int i=0; i<params.length; i++){
+                preparedStatement.setObject(i+1, params[i]);
+            }
+            return preparedStatement;
+        }, keyHolder);
+        user.setId(keyHolder.getKey().intValue());
     }
 
     public void update(User user) {
         Object[] params = new Object[]{user.getName(), user.getPassword(), user.getId()};
         String sql = "update userinfo set name = ?, password = ? where id = ?";
-        jdbcContext.updateOrDelete(sql, params);
+        jdbcTemplate.update(sql, params);
     }
 
     public void delete(Integer id) {
         Object[] params = new Object[]{id};
         String sql = "delete from userinfo where id = ?";
-        jdbcContext.updateOrDelete(sql, params);
+        jdbcTemplate.update(sql, params);
     }
 }
